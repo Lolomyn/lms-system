b702bee4d68ad4382d50d0331dd90574d95910ce
Savelev Maxim <vismanmark@yandex.ru>
2025-06-24 19:49:44 +0300

    create models

--- lms/models.py
+++ lms/models.py
@@ -1,9 +1,29 @@
 from django.db import models
 
 
-class Course(models.Model):
-    pass
-
-
 class Lesson(models.Model):
-    pass
+    title = models.CharField(max_length=250, verbose_name='Название')
+    description = models.TextField(blank=True, null=True, verbose_name='Описание')
+    preview = models.ImageField(upload_to='photos/courses', verbose_name='Превью')
+    video_url = models.URLField(verbose_name='Ссылка на видео')
+
+    class Meta:
+        verbose_name = 'Урок'
+        verbose_name_plural = 'Уроки'
+
+    def __str__(self):
+        return self.title
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=250, verbose_name='Название')
+    preview = models.ImageField(upload_to='photos/courses', verbose_name='Превью')
+    description = models.TextField(blank=True, null=True, verbose_name='Описание')
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, verbose_name='Урок')
+
+    class Meta:
+        verbose_name = 'Курс'
+        verbose_name_plural = 'Курсы'
+
+    def __str__(self):
+        return self.title
