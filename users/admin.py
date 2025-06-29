545c0fce4226e5f3a1b6d3ecea67913f68455cae
Savelev Maxim <vismanmark@yandex.ru>
2025-06-29 14:53:28 +0300

    add custom command add_payments

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib.auth.admin import UserAdmin
+
+from .models import User
+
+
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ('id', 'email', 'phone', 'country', 'avatar')
+    list_filter = ('email', )
+    fieldsets = (
+        (None, {'fields': ('email', 'avatar', 'phone', 'country')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'groups', 'user_permissions')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email', 'password1', 'password2', 'avatar', 'phone', 'country')}
+         ),
+    )
+
+    search_fields = ('email', )
+    ordering = ('email',)
+
+
+admin.site.register(User, CustomUserAdmin)
--- users/models.py
+++ users/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+from lms.models import Course
+
 
 class User(AbstractUser):
     username = None
@@ -20,3 +22,23 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.email
+
+
+class Payment(models.Model):
+    PAYMENT_CHOICES = [
+        ('cash', 'Cash'),
+        ('remittance', 'Remittance'),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Пользователь')
+    payment_date = models.DateField(verbose_name='Дата оплаты')
+    payment_course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='Оплаченный курс')
+    payment_sum = models.PositiveIntegerField(verbose_name='Сумма оплаты')
+    payment_method = models.CharField(choices=PAYMENT_CHOICES, default='remittance', verbose_name='Способ оплаты')
+
+    class Meta:
+        verbose_name = 'Оплата'
+        verbose_name_plural = 'Оплаты'
+
+    def __str__(self):
+        return f"{self.payment_course} / {self.payment_date}: {self.payment_sum} руб."
