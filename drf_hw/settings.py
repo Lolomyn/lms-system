4c0706a3b95d2cfc637e1b2e73649c6c5c905012
Savelev Maxim <vismanmark@yandex.ru>
2025-07-27 11:26:48 +0300

    установка django-celery-beat для работы с периодическими задачами

--- drf_hw/settings.py
+++ drf_hw/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'django_celery_beat',
     "drf_yasg",
     "rest_framework",
     "django_filters",
